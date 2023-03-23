4101d159b3aeaa460f5799721fd524e249c5ff7e
Nwankwo Gabriel Ogechi <nwankwogabriel123@gmail.com>
2023-03-23 06:59:05 -0700

    updated

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
-#include "main.h" 
-  
- /** 
-  * add - adds two integers 
-  * @a: first integer 
-  * @b: second integer 
-  * Return: sum of the two integers 
-  */ 
- int add(int a, int b) 
- { 
- return (a + b); 
- }
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of the two integers
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
